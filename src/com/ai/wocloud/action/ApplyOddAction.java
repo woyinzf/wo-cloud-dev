823fb9a420faa4cd4881b37bb75fa6e3f008f030
yinzf <woyinzf@163.com>
2016-05-06 08:15:28 +0800

    test commit 2

--- src/com/ai/wocloud/action/ApplyOddAction.java
+++ src/com/ai/wocloud/action/ApplyOddAction.java
@@ -75,6 +75,7 @@ public class ApplyOddAction extends BaseAction {
 	@SuppressWarnings("unchecked")
     @RequestMapping(value = "/addApplyOdd")
 	public void addApplyOdd(Software software,HttpServletRequest request, HttpServletResponse response){
+		System.out.println("test 456789");
 		String userId=SessionInfo.getLoginInfo(request).getLoginName();
 		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String dateStr = dateformat.format(new Date());
