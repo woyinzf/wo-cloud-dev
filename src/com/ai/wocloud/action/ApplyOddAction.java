fee2e37de753f42e0103318f7b4fc8417309cac3
yinzf <woyinzf@163.com>
2016-05-09 11:31:47 +0800

    test put and get

--- src/com/ai/wocloud/action/ApplyOddAction.java
+++ src/com/ai/wocloud/action/ApplyOddAction.java
@@ -76,6 +76,7 @@ public class ApplyOddAction extends BaseAction {
     @RequestMapping(value = "/addApplyOdd")
 	public void addApplyOdd(Software software,HttpServletRequest request, HttpServletResponse response){
 		System.out.println("test 456789");
+		System.out.println("test 1111");
 		String userId=SessionInfo.getLoginInfo(request).getLoginName();
 		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String dateStr = dateformat.format(new Date());
