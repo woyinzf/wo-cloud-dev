0319097da5fcbb68d73ea7f9c576f00a5b38a5b5
yinzf <woyinzf@163.com>
2016-05-09 15:01:34 +0800

    测试合并4

--- src/com/ai/wocloud/action/ApplyOddAction.java
+++ src/com/ai/wocloud/action/ApplyOddAction.java
@@ -86,7 +86,7 @@ public class ApplyOddAction extends BaseAction {
 		JSONObject json = new JSONObject();
 		String POST_URL="";
 		List<String> fileName = new ArrayList<String>();
-		System.out.println("test 888888888");
+		System.out.println("test 666666666666");
 		try {
 			POST_URL = GetInterfacesUrlUtils.getInterfacesUrl("vmUrl");
 		} catch (Exception e) {
